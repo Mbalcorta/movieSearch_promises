67d768b41c206434aad472f514e2d0942f9f20c9
Mateo Balcorta <misterrattail@gmail.com>
2017-09-25 11:40:53 -0700

    added test.

--- app/app.js
+++ app/app.js
@@ -1,19 +1,64 @@
-var rp = require('request-promise');
+const rp = require('request-promise');
 
-var cheerio = require('cheerio'); // Basically jQuery for node.js
+const cheerio = require('cheerio'); // Basically jQuery for node.js
 
-var options = {
-    uri: 'http://www.imdb.com',
-    path: '/find?ref_=nv_sr_fn&q=findingnemo&s=all',
-    transform: function (body) {
-        return cheerio.load(body);
-    }
-};
+const movieTitle = process.argv[2]
 
-rp(options)
-    .then(function ($) {
-        // Process html like you would with jQuery...
+const options = {
+  uri: `http://www.imdb.com/find?ref_=nv_sr_fn&q=${movieTitle}&s=all`,
+  transform : function(body){
+      return cheerio.load(body)
+  }
+}
+
+const movieSearch = (movieTitle) => {
+  if(movieTitle) {
+  rp(options)
+    .then(($) => {
+      return parseData($)
     })
-    .catch(function (err) {
-        // Crawling failed or Cheerio choked...
+    .then((textArray) => {
+      return printText(textArray)
+    })
+    .catch((err) => {
+      throw new Error(err)
     });
+  } else {
+    throw new Error('Error: must enter movie title')
+  }
+}
+
+if(!module.parent){
+  try {
+    movieSearch(movieTitle)
+  } catch(e){
+    console.log(`${e.message}`)
+  }
+}
+
+const parseData = ($) => {
+  $('small').remove()
+  const textArray =
+    $('.findList')
+    .first()
+    .find('.result_text')
+    .map((i, element)=>{
+      return $(element).text()
+    }).toArray()
+  return textArray;
+}
+
+const printText = (textArray) => {
+  try{
+    textArray.forEach((eachMovieTitle) => {
+      if(!module.parent){
+         console.log(eachMovieTitle)
+      }
+    })
+  } catch(e){
+    throw new Error('Error: Cannot print Text')
+  }
+
+}
+
+module.exports = {options, parseData, printText, movieSearch, parseData, rp, cheerio}
--- test/test.js
+++ test/test.js
@@ -1,2 +1,93 @@
 const expect = require('chai').expect
 const mocha = require('mocha')
+const {parseData, printText, movieSearch, parsedata, rp, cheerio} = require('../app/app.js')
+
+
+describe('Movie search function', function() {
+  context('when movie name not entered', function() {
+
+      it('will throw an error', function() {
+          expect(movieSearch).to.throw(Error)
+      })
+    })
+context('when movie name entered', function() {
+    it('will now throw an error', function() {
+      expect(function(){
+        movieSearch('findingnemo')
+      }).to.not.throw(Error)
+    })
+  })
+})
+
+describe('Parse data function', function() {
+  const options = {
+    uri: `http://www.imdb.com/find?ref_=nv_sr_fn&q=findingnemo&s=all`,
+    transform : function(body){
+        return cheerio.load(body)
+    }
+  }
+
+  context('When data is parsed', function() {
+      it('will return html without small tags', function() {
+        return rp(options)
+        .then(
+          function($){
+            parseData($)
+            expect($('small').html()).to.be.an('null')
+          }
+        )
+      })
+    })
+  context('when movie title passed in', function() {
+      it('will return an array', function() {
+        return rp(options)
+        .then(parseData)
+        .then(
+          function(arrayOfTitles){
+          expect(arrayOfTitles).to.be.an('array')
+        })
+      })
+    })
+  context('when movie title passed in', function() {
+      it('will return an array containing movie title strings', function() {
+        return rp(options)
+        .then(parseData)
+        .then(
+          function(arrayOfTitles){
+          expect(arrayOfTitles[0]).to.contain('Finding Nemo')
+        })
+      })
+    })
+})
+
+describe('Print text function', function() {
+  const options = {
+    uri: `http://www.imdb.com/find?ref_=nv_sr_fn&q=findingnemo&s=all`,
+    transform : function(body){
+        return cheerio.load(body)
+    }
+  }
+
+  context('When array of strings passed in ', function() {
+      it('will not throw an error', function() {
+        return rp(options)
+        .then(function($){
+          return parseData($)
+        })
+        .then(function(arrayOfStrings){
+          console.log(printText)
+          expect(function(){
+            printText(arrayOfStrings)
+          }).to.not.throw('Cannot print Text')
+        })
+      })
+    })
+
+  context('When numbers passed in ', function() {
+    it('will throw an error', function() {
+        expect(function(){
+          printText(123)
+        }).to.throw('Error: Cannot print Text')
+      })
+    })
+})
